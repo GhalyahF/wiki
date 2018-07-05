4d5b43e72edf99a7d7dc72b43585b4397ab66844
GhalyahF <gaaa291@nyu.edu>
2018-07-05 13:16:37 +0300

    fixed templates

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Page(models.Model):
+	title= models.CharField(max_length=200)
+	content= models.TextField()
+	last_update= models.DateTimeField(auto_now_add= True)
+
+	def __str__(self):
+		return self.title
+
+	def absolute_url_view(request):
+		return reverse('pages.views.details', args=str[(self.id)])
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Page
 
 # Create your views here.
+def list(request):
+	context={
+			"pages": Page.objects.all(),
+	}
+	return render (request,'list.html',context)
+
+def detail(request,page_id):
+	context={
+			"page": Page.objects.get(id=page_id),
+
+	}
+	return render (request,'detail.html',context)
+
+
+
